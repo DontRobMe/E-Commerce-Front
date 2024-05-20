1195ce2006bd28d09b7c9ec6b08304d992f3fbf8
Gourbal François <francois.gourbal@ynov.com>
2024-05-20 18:30:22 +0200

    MODIFICATIO ET REDIRECCTIONS SUR LOGIN ET register.component.html

--- EcommerceFront/tailwind.config.js
+++ EcommerceFront/tailwind.config.js
@@ -4,11 +4,20 @@ module.exports = {
     './src/**/*.html',
     './src/**/*.vue',
     './src/**/*.jsx',
-
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': '#1877f2',
+        'custom-white': '#fff',
+        'custom-black': '#000',
+        'custom-blue-2': '#5865f2',
+        'custom-blue-hover': '#3b49df',
+        'custom-blue-hover-2': '#1e2b8f',
+        'custom-white-hover': '#f0f2f5',
+        'custom-black-hover': '#1c1e21',
+      },
+    },
   },
   plugins: [],
 }
-
