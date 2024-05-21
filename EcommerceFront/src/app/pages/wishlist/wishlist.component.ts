5b2eb7c78aba7d8c2a649cca82997461388509dd
mamadou <amadou.mbodj@ynov.com>
2024-05-21 19:16:30 +0200

    b

--- EcommerceFront/src/app/pages/wishlist/wishlist.component.ts
+++ EcommerceFront/src/app/pages/wishlist/wishlist.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {ProductService} from "../../services/produits/produit.service";
+import {UserService} from "../../services/users/user.service";
 
 @Component({
   selector: 'app-wishlist',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './wishlist.component.html',
 })
 export class WishlistComponent {
+
+  constructor( private route: ActivatedRoute,
+               private productService: ProductService,
+               private userService: UserService,
+               private router: Router) {
+  }
+
+
 
 }
